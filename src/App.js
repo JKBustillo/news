7f2da558a0319da26a14d210c5bc0ece6c1a3cca
Jose Bustillo <jabustillo@uninorte.edu.co>
2020-04-09 21:16:39 -0500

    Noticia and ListadoNoticia

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Form from './components/Form';
+import ListadoNoticias from './components/ListadoNoticias';
 
 function App() {
   const [categoria, setCategoria] = useState('');
+  const [noticias, setNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `http://newsapi.org/v2/top-headlines?country=co&category=${categoria}&apiKey=42de2bd7c4f54870ac8827df9314866f`;
+      const respuesta = await fetch(url);
+      const noticias = await respuesta.json();
+      setNoticias(noticias.articles);
+      
+    };
+    consultarAPI();
+  }, [categoria]);
+
   return (
     <Fragment>
       <Header titulo="Buscador de noticias"/>
@@ -11,6 +25,8 @@ function App() {
         <Form
           setCategoria={setCategoria}
         />
+
+        <ListadoNoticias noticias={noticias} />
       </div>
     </Fragment>
   );
